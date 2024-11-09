72cb3a542a3959f8fc57fb3cc4154f7af68259f2
Harish kumar A <harishkumar.a6@gmail.com>
2024-11-09 17:36:19 +0530

    Completed Catalog.API implementation

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Catalog.API.Products.GetProductById
+{
+    //public record GetProductByIdRequest();
+    public record GetProductByIdResponse(Product Product);
+    public class GetProductByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                var result  = await sender.Send(new GetProductByIdQuery(id));
+                var product = result.Adapt<GetProductByIdResponse>();
+                return Results.Ok(product);
+            }).WithName("GetProductById")
+        .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Get Product By Id")
+        .WithDescription("Get Product By Id");
+        }
+    }
+}
